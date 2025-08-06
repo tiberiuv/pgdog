8f31e466287dbae30bec7d790cb7a14638c5670d
Lev Kokotov <levkk@users.noreply.github.com>
2025-08-06 13:52:56 -0700

    Fix deadlock in show query_cache (#310)

--- pgdog/src/admin/show_query_cache.rs
+++ pgdog/src/admin/show_query_cache.rs
@@ -27,7 +27,7 @@ impl Command for ShowQueryCache {
     }
 
     async fn execute(&self) -> Result<Vec<Message>, Error> {
-        let queries = Cache::queries();
+        let mut queries: Vec<_> = Cache::queries().into_iter().collect();
         let mut messages = vec![RowDescription::new(&[
             Field::text("query"),
             Field::numeric("hits"),
@@ -36,22 +36,59 @@ impl Command for ShowQueryCache {
         ])
         .message()?];
 
-        let mut queries: Vec<_> = queries.into_iter().collect();
-        queries.sort_by_key(|v| v.1.stats.lock().hits);
+        queries.sort_by_cached_key(|v| v.1.stats.lock().hits);
 
         for query in queries.into_iter().rev() {
             if !self.filter.is_empty() && !query.0.to_lowercase().contains(&self.filter) {
                 continue;
             }
             let mut data_row = DataRow::new();
+            let stats = query.1.stats.lock().clone();
             data_row
                 .add(query.0)
-                .add(query.1.stats.lock().hits)
-                .add(query.1.stats.lock().direct)
-                .add(query.1.stats.lock().multi);
+                .add(stats.hits)
+                .add(stats.direct)
+                .add(stats.multi);
             messages.push(data_row.message()?);
         }
 
         Ok(messages)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::net::{FromBytes, ToBytes};
+
+    use super::*;
+
+    #[tokio::test]
+    async fn test_show_query_cache() {
+        let cache = Cache::get();
+
+        for q in 0..5 {
+            cache
+                .parse(format!("SELECT $1::bigint, {}", q).as_str())
+                .unwrap();
+        }
+
+        let show = ShowQueryCache {
+            filter: String::new(),
+        }
+        .execute()
+        .await
+        .unwrap();
+
+        let mut total = 0;
+        for message in show {
+            if message.code() == 'D' {
+                total += 1;
+                let data_row = DataRow::from_bytes(message.to_bytes().unwrap()).unwrap();
+                let hits = data_row.get_int(1, true).unwrap();
+                assert_eq!(hits, 1);
+            }
+        }
+
+        assert_eq!(total, 5);
     }
 }
